c9e1ffccdb940e8adb8bed8f461c9ba3529fe9a0
Gabriel Calixto <gabriel_calixto.gsc@hotmail.com>
2020-10-29 16:41:13 -0300

    conexão com o banco de dados na nuvem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ObjectId = mongodb.ObjectId;
 
 (async () => {
 
-    const connectionString = 'mongodb://localhost:27017';
+    const connectionString = 'mongodb+srv://admin:admin@cluster0.wujr8.mongodb.net/ocean_mongodb?retryWrites=true&w=majority';
 
     console.info('Conectando ao banco de dados MongoDB...');
 
@@ -13,7 +13,7 @@ const ObjectId = mongodb.ObjectId;
         useUnifiedTopology: true
     };
 
-    //const client = await mongodb.MongoClient.connect(connectionString, options);
+    const client = await mongodb.MongoClient.connect(connectionString, options);
 
     console.info('MongoDB conectado com sucesso!');
 
@@ -57,67 +57,67 @@ const ObjectId = mongodb.ObjectId;
     app.get('/', function (req, res) {
         res.send('Hello World');
     });
-    /*
-    const db = client.db('ocean_backend_27_10_2020');
+
+    const db = client.db('ocean_mongodb');
     const mensagens = db.collection('mensagens');
-    
+
     // Read all
     app.get('/mensagem', async function (req, res) {
         const findResult = await mensagens.find().toArray();
-    
+
         res.send(findResult);
     });
-    
+
     // Create
     app.post('/mensagem', async function (req, res) {
         const texto = req.body.texto;
-    
+
         const mensagem = {
             texto
         };
-    
+
         const resultado = await mensagens.insertOne(mensagem);
-    
+
         const objetoInserido = resultado.ops[0];
-    
+
         res.send(objetoInserido);
     });
-    
+
     // Read Single
     app.get('/mensagem/:id', async function (req, res) {
         const id = req.params.id;
-    
+
         const mensagem = await mensagens.findOne({ _id: ObjectId(id) });
-    
+
         res.send(mensagem);
     });
-    
+
     // Update
     app.put('/mensagem/:id', async function (req, res) {
         const id = req.params.id;
-    
+
         const mensagem = {
             _id: ObjectId(id),
             ...req.body
         };
-    
+
         await mensagens.updateOne(
             { _id: ObjectId(id) },
             { $set: mensagem }
         );
-    
+
         res.send(mensagem);
     });
-    
+
     // Delete
     app.delete('/mensagem/:id', async function (req, res) {
         const id = req.params.id;
-    
+
         await mensagens.deleteOne({ _id: ObjectId(id) });
-    
+
         res.send(`A mensagem de ID ${id} foi removida com sucesso.`);
     });
-    */
+
     app.listen(port, function () {
         console.info('App rodando em http://localhost:' + port);
     });
